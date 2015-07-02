c529828f483fd93f2e9ae21b57d3fe63543fe940
methodswithclass <chris@methodswithclass.com>
2015-07-02 18:03:55 -0400

    route otherwise

--- js/app.js
+++ js/app.js
@@ -4,16 +4,21 @@ var mobiledebug = false;
 
 app.config(function($routeProvider) {
     $routeProvider.
-      //Display desktop version
+
       when('/invalid', {
-        //Template for Desktop based browsers
+      
         templateUrl: 'views/invalid.html'
       }).
-      //Display mobile version
+
       when('/valid', {
-        //Template for Mobile based browsers
+      
         templateUrl: 'views/valid.html'
-      }).otherwise("views/checking.html");
+      }).
+
+      otherwise("/checking",{
+
+      	templateUrl:'views/checking.html'
+      });
 }).
 run(function (validate, events, $location) {
 
