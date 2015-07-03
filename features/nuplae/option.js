945c868d972cf43f93a1b0ec1895cd7fe2d32683
methodswithclass <chris@methodswithclass.com>
2015-07-03 12:30:17 -0400

    option tapped

--- features/nuplae/option.js
+++ features/nuplae/option.js
@@ -1,16 +1,22 @@
 nuplaeModule.directive("option", function () {
 
-	var directive = {
+	return {
 		restrict:'E',
 		replace:true,
-		template:"<div class='relative width80 height-100 margin-v-20 center border {{info.page.menu}}'>" +
-					"<div class='absolute center font-30 text-center white'>{{info.name}}</div>" + 
-				 "</div>",
 		scope:{
 			info:'='
 		},
-	};
+		template:"<div class='relative width80 height-100 margin-v-20 center border {{info.page.menu}}' on-tap='tapped(info)'>" +
+					"<div class='absolute center font-30 text-center white'>{{info.name}}</div>" + 
+				 "</div>",
+		link:function ($scope, element, attr) {
 
-	return directive;
+			$scope.tapped = function (info) {
+
+				console.log("tapped " + info.name);
+			}
+
+		}
+	}
 
 });
