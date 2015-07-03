a1475885e491f3ef054dd7f47ec926ace8fdcaee
methodswithclass <chris@methodswithclass.com>
2015-07-03 19:35:42 -0400

    inject navigation

--- features/nuplae/page.js
+++ features/nuplae/page.js
@@ -1,4 +1,4 @@
-nuplaeModule.directive("page", function () {
+nuplaeModule.directive("page", ['navigation', function (nav) {
 
 	return {
 		restrict:'E',
@@ -21,4 +21,4 @@ nuplaeModule.directive("page", function () {
 		}
 
 	}
-});
+}]);
