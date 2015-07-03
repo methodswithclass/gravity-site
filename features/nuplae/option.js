f739cea6be117bdc3e78451c063a77efcb5ab59b
methodswithclass <chris@methodswithclass.com>
2015-07-03 12:19:33 -0400

    option borders

--- features/nuplae/option.js
+++ features/nuplae/option.js
@@ -4,7 +4,7 @@ nuplaeModule.directive("option", function () {
 		restrict:'E',
 		replace:true,
 		template:"<div class='absolute width80 margin-v-20 height-100 center border {{info.page.menu}}'>" +
-					"<div class='absolute center font-30 text-center'>{{info.name}}</div>" + 
+					"<div class='absolute center font-30 text-center white'>{{info.name}}</div>" + 
 				 "</div>",
 		scope:{
 			info:'='
