8380c22f5695df7a1b39d92738acadca945771c1
methodswithclass <chris@methodswithclass.com>
2015-07-02 21:21:46 -0400

    validate

--- js/acceleration/validate-service.js
+++ js/acceleration/validate-service.js
@@ -7,6 +7,7 @@ accelModule.factory("validate", function ($q) {
 
 	var check = 0;
 	this.motion = false;
+	this.checkMotion = true;
 
 	var setMotion = function (motion) {
 
@@ -19,6 +20,8 @@ accelModule.factory("validate", function ($q) {
 	}
 
 	var checkSupported = function (resolve, reject) {
+
+		self.checkMotion = true;
 
 		if(isMotion()) {
 
@@ -42,24 +45,23 @@ accelModule.factory("validate", function ($q) {
 
 			window.addEventListener("devicemotion", function (e) {
 
-				//console.log(e.accelerationIncludingGravity.x);
-				if (check < 5) {
+				if (self.checkMotion) {
+
 					if (e.accelerationIncludingGravity.x || e.acceleration.x) {
 						console.log("DeviceMotion is supported: " + e.accelerationIncludingGravity.x);
 						setMotion(true);
+						checkSupported(resolve, reject);
 					}
 					else {
 						console.log("DeviceMotion is not supported");
 						setMotion(false);
+						check++;
+
+						if (check > 10) {
+							checkSupported(resolve, reject);
+						}
 					}
-					check++;
-				}
-				else if (check == 5) {
 
-					checkSupported(resolve, reject);
-
-					check++;
-					
 				}
 
 			});
