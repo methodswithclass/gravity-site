d7ed3b25cf5bd47f52ef5467c7314c7b0f98d0a7
methodswithclass <chris@methodswithclass.com>
2015-07-03 12:16:26 -0400

    option button

--- features/nuplae/option.js
+++ features/nuplae/option.js
@@ -3,7 +3,7 @@ nuplaeModule.directive("option", function () {
 	var directive = {
 		restrict:'E',
 		replace:true,
-		template:"<div class='absolute width text-center vcenter border-blue'>{{info.name}}</div>",
+		template:"<div class='absolute width60 margin-v-20 height-100 center font-40 text-center border {{info.page.menu}}'>{{info.name}}</div>",
 		scope:{
 			info:'='
 		},
