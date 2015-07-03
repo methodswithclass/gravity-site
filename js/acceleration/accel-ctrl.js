f64e18cb4a52ca12f5a0d04b8c01517527b65b78
methodswithclass <chris@methodswithclass.com>
2015-07-02 21:15:13 -0400

    styles, $q in run, clean up controller

--- js/acceleration/accel-ctrl.js
+++ js/acceleration/accel-ctrl.js
@@ -1,43 +1,9 @@
-accelModule.controller('accel-ctrl', ['$scope', '$location', 'validate', 'events', '$document', 'con', function ($scope, $location, validate, events, $document, con) {
+accelModule.controller('accel-ctrl', ['$document', 'con', function ($document, con) {
 
 	var self = this;
 
-	// var valid = "/valid";
-	// var invalid = "/invalid";
 
-	// var isValid = {};
-	// var time = 0;
-	// this.timer;
 
-	// var check = function () {
-
-	// 	self.timer = setInterval(function () {
-
-	// 		time += 10;
-
-	// 		isValid = events.dispatch('validate');
-
-	// 		$scope.production = isValid.route;
-
-	// 		if (isValid.done || time > 1000) {
-	// 			validate.stop();
-	// 			clearInterval(self.timer);
-	// 		}
-
-	// 	}, 10);
-	// }
-
-	// validate.run();
-
-	// check();
-
-	// $scope.$watch(function (scope) {
-
-	// 	return scope.production;
-	// }, function (newValue, oldValue) {
-
-	// 	$location.path(newValue);
-	// });
 
 	angular.element($document).ready(function () {
 
--- js/app.js
+++ js/app.js
@@ -10,23 +10,21 @@ app.config(function($routeProvider) {
         templateUrl: 'views/invalid.html'
 
       }).
-
       when('/valid', {
       
         templateUrl: 'views/valid.html'
       }).
-
       otherwise("/checking",{
 
       	templateUrl:'views/checking.html'
       });
 }).
-run(function ($location, $q, validate) {
+run(function ($location, validate) {
 
 	var self = this;
 
 	var isValid = validate.run();
-	
+
 	isValid.then(	
 	function (path) {
 		$location.path(path);
