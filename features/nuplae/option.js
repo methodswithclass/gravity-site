40a494de0c4d06257f8fb611ab887aea177b7582
methodswithclass <chris@methodswithclass.com>
2015-07-02 23:40:43 -0400

    option

--- features/nuplae/option.js
+++ features/nuplae/option.js
@@ -4,7 +4,7 @@ nuplaeModule.directive("option", function () {
 		scope:{
 			info:'='
 		},
-		template:'<div class="relative width80 border {{info.page.menu}}" ng-tap="onTap(info)">',
+		template:'<div class="relative margin-20 width80 border text-center {{info.page.menu}}" ng-tap="onTap(info)">{{info.name}}</div>',
 		link:function ($scope, element, attr) {
 
 			$scope.onTap = function (info) {
