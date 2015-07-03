45e2f68e08a01c60c72223c747ec02d42988d97a
methodswithclass <chris@methodswithclass.com>
2015-07-03 01:08:26 -0400

    print data

--- features/nuplae/nuplae-ctrl.js
+++ features/nuplae/nuplae-ctrl.js
@@ -1,8 +1,19 @@
-nuplaeModule.controller('nuplae-ctrl', ['$document', 'con', 'params', function ($document, con, pages) {
+nuplaeModule.controller('nuplae-ctrl', ['$document', 'con', 'params', function ($document, con, params) {
 
 	var self = this;
 
-	this.data = pages;
+	this.data = {};
+
+	this.data.home = params.home;
+
+	this.data.pages = params.pages;
+
+	console.log(params.pages.length);
+
+	for (i in params.pages) {
+
+		console.log(params.pages[i].name);
+	}
 
 	angular.element($document).ready(function () {
 
