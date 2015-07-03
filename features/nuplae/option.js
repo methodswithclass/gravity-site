9ff280a43b48ed0ab6c447d387a6e810b3c4cfa3
methodswithclass <chris@methodswithclass.com>
2015-07-03 11:51:34 -0400

    option classes

--- features/nuplae/option.js
+++ features/nuplae/option.js
@@ -1,8 +1,10 @@
 nuplaeModule.directive("option", function () {
 
+	var classes = "{'absolute', 'width', 'height30', 'text-center', 'vcenter'}";
+
 	var directive = {
 		restrict:'E',
-		template:"<div ng-class=[absolute, width, height30, text-center, vcenter]>{{info.name}}</div>",
+		template:"<div ng-class=classes>{{info.name}}</div>",
 		scope:{
 			info:'='
 		},
