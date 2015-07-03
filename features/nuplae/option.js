280f4e8f56efd3c238ccbc3ad4bfc8cc1b652d1f
methodswithclass <chris@methodswithclass.com>
2015-07-03 12:10:29 -0400

    option border remove option.html

--- features/nuplae/option.js
+++ features/nuplae/option.js
@@ -3,7 +3,7 @@ nuplaeModule.directive("option", function () {
 	var directive = {
 		restrict:'E',
 		replace:true,
-		template:"<div class='absolute width height30 text-center vcenter'>{{info.name}}</div>",
+		template:"<div class='absolute width height30 text-center vcenter border-blue'>{{info.name}}</div>",
 		scope:{
 			info:'='
 		},
