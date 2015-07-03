a5f0aa31817bbe802286b285c18f8160aa699b95
methodswithclass <chris@methodswithclass.com>
2015-07-03 17:48:09 -0400

    init in controller

--- features/nuplae/nuplae.ctrl.js
+++ features/nuplae/nuplae.ctrl.js
@@ -7,6 +7,42 @@ nuplaeModule.controller('nuplaeCtrl', ['$document', 'con', 'params', function ($
 	console.log(self.pages[0].name);
 
 
+
+	var init = function () {
+
+    	var home;
+
+    	var body;
+
+    	var complete = function () {
+
+    		console.log("loaded");
+
+    		var body = $("#body");
+
+			console.log(home[0]);
+
+			console.log(body[0]);
+
+			body.animate({top:0, left:"25%"},10);
+    	}
+
+    	var timer = setInterval(function () {
+
+    		home = $("#pageHome");
+
+    		if (home[0]) {
+    			clearInterval(timer);
+    			complete();
+    		}
+
+    	}, 10);
+
+	}
+
+    init();
+
+
 	angular.element($document).ready(function () {
 
 		con.register($("#consoleContainer"));
--- features/nuplae/page.js
+++ features/nuplae/page.js
@@ -18,44 +18,6 @@ nuplaeModule.directive("page", function () {
                 return 'features/nuplae/' + view;
             }
 
-            var init = function () {
-
-            	if ($scope.info.name == "Home") {
-
-			    	var home;
-
-			    	var body;
-
-			    	var complete = function () {
-
-			    		console.log("loaded");
-
-			    		var body = $("#body");
-
-						console.log(home[0]);
-
-						console.log(body[0]);
-
-						body.animate({top:0, left:"25%"},10);
-			    	}
-
-			    	var timer = setInterval(function () {
-
-			    		home = $("#pageHome");
-
-			    		if (home[0]) {
-			    			clearInterval(timer);
-			    			complete();
-			    		}
-
-			    	}, 10);
-
-		    	}
-		    }
-
-		    init();
-
-
 		}
 
 	}
