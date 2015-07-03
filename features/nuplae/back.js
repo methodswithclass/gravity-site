2e0d50334cea9bf58390881a16ef7bbca1519dd3
methodswithclass <chris@methodswithclass.com>
2015-07-03 19:20:06 -0400

    back

--- features/nuplae/back.js
+++ features/nuplae/back.js
@@ -3,7 +3,7 @@ nuplaeModule.directive("back", ['navigation', function (nav) {
 	return {
 		restrict:'E',
 		replace:true,
-		template:"<div class='absolute bottom-100 width60 height-50 gray-back hcenter' on-tap='tapped()'><div class='absolute center text-center font-30 white'>Back</div></div>",
+		template:"<div class='absolute bottom-100 width60 height-50 gray-back hcenter pointer' on-tap='tapped()'><div class='absolute center text-center font-30 white'>Back</div></div>",
 		link:function ($scope, element, attr) {
 
 			$scope.tapped = function () {
