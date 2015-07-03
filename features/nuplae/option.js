48eff91c83e726a1868f8ece819344dd763d83ad
methodswithclass <chris@methodswithclass.com>
2015-07-03 12:25:13 -0400

    option relative

--- features/nuplae/option.js
+++ features/nuplae/option.js
@@ -3,7 +3,7 @@ nuplaeModule.directive("option", function () {
 	var directive = {
 		restrict:'E',
 		replace:true,
-		template:"<div class='absolute width80 height-100 center border {{info.page.menu}}'>" +
+		template:"<div class='relative width80 height-100 margin-v-20 center border {{info.page.menu}}'>" +
 					"<div class='absolute center font-30 text-center white'>{{info.name}}</div>" + 
 				 "</div>",
 		scope:{
