2cb7c88ff80a3ae4af99762b0dcf3f879843fb8f
methodswithclass <chris@methodswithclass.com>
2015-07-03 17:45:35 -0400

    init to directive

--- features/nuplae/nuplae.ctrl.js
+++ features/nuplae/nuplae.ctrl.js
@@ -6,40 +6,6 @@ nuplaeModule.controller('nuplaeCtrl', ['$document', 'con', 'params', function ($
 
 	console.log(self.pages[0].name);
 
-	var init = function () {
-
-    	var home;
-
-    	var body;
-
-    	var complete = function () {
-
-    		console.log("loaded");
-
-    		body = $("#body");
-
-			console.log(home[0]);
-
-			console.log(body[0]);
-
-			body.scrollTo(home,10);
-    	}
-
-    	var timer = setInterval(function () {
-
-    		home = $("#pageHome");
-
-    		if (home[0]) {
-    			clearInterval(timer);
-    			complete();
-    		}
-
-    	}, 10);
-    }
-
-
-    init();
-
 
 	angular.element($document).ready(function () {
 
