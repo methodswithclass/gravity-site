2e4ba0e684819ef13670d88daf6d8c4df2d906e1
methodswithclass <chris@methodswithclass.com>
2015-07-03 23:31:45 -0400

    font size

--- features/nuplae/option.js
+++ features/nuplae/option.js
@@ -7,7 +7,7 @@ nuplaeModule.directive("option", ['nuplaeService', 'navigation', function (nuSer
 			info:'='
 		},
 		template:"<div class='relative width90 height-150 margin-v-50 center border pointer {{info.menu}}'>" +
-					"<div class='absolute center font-40 text-center white' id={{'title' + info.name}}>{{info.name}}</div>" + 
+					"<div class='absolute center font-50 text-center white' id={{'title' + info.name}}>{{info.name}}</div>" + 
 				 "</div>",
 		link:function ($scope, element, attr) {
 
