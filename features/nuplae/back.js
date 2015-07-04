4ff896cf189f4abceba09bd58b636cd4aa7b2a74
methodswithclass <chris@methodswithclass.com>
2015-07-04 16:26:40 -0400

    back

--- features/nuplae/back.js
+++ features/nuplae/back.js
@@ -6,7 +6,7 @@ nuplaeModule.directive("back", ['navigation', 'nuplaeService', function (nav, nu
 		},
 		restrict:'E',
 		replace:true,
-		template:"<div ng-class='getClasses(game)' ng-style='getimgloc(game)'><img src='/img/back.png' class='absolute width height corner'/></div>",
+		template:"<i ng-class='getClasses(game)' ng-style='getimgloc(game)'></i>",
 		link:function ($scope, element, attr) {
 
 			$scope.getimgloc = function (info) {
@@ -25,16 +25,16 @@ nuplaeModule.directive("back", ['navigation', 'nuplaeService', function (nav, nu
 
 			$scope.getClasses = function (info) {
 
-				var classes = "absolute margin-10 width-100 height-100 pointer gray-back rounded20";
+				var classes = "fa fa-chevron-right fa-4x";
 				var rect = info.page.rect;
 
 				//console.log("rotate " + rect.top + " " + rect.left);
 
 				if (rect.top == 0 && rect.left == 0) {
-					classes += ' flip-h';
+					classes += ' fa-flip-horizontal';
 				}
 				else if (rect.top == "50%") {
-					classes += ' rotate-clock-90';
+					classes += ' fa-flip-horizontal fa-rotate-90';
 				}
 
 				return classes;
