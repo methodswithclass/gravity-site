50e5d1f727e82ef0e52e2f5a5cbc7ed63c0a7ed0
methodswithclass <chris@methodswithclass.com>
2015-07-04 13:49:25 -0400

    print page data

--- features/nuplae/page.js
+++ features/nuplae/page.js
@@ -9,6 +9,8 @@ nuplaeModule.directive("page", function () {
 		template:'<div ng-include="getContentUrl()"></div>',
 		link:function ($scope, element, attr) {
 
+			console.log("name: " $scope.info.name + " view: " + $scope.view);
+
 			$scope.getContentUrl = function() {
 				//console.log($scope.view);
                 return 'features/nuplae/' + $scope.view;
