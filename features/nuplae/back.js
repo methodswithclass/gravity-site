ab9010042e1658396e9c2af5ae9391d7ae2d1c94
methodswithclass <chris@methodswithclass.com>
2015-07-04 16:47:42 -0400

    back

--- features/nuplae/back.js
+++ features/nuplae/back.js
@@ -11,13 +11,13 @@ nuplaeModule.directive("back", ['navigation', 'nuplaeService', function (nav, nu
 
 			$scope.getimgloc = function (info) {
 
-				var style = {top:0, left:0};
+				var style = {top:0, left:"5px"};
 				var rect = info.page.rect;
 
 				//console.log("location " + rect.top + " " + rect.left);
 
 				if (rect.left == 0) {
-					style = {top:0, right:0};
+					style = {top:0, right:"5px"};
 				}
 
 				return style;
@@ -34,7 +34,7 @@ nuplaeModule.directive("back", ['navigation', 'nuplaeService', function (nav, nu
 					classes += ' flip-h';
 				}
 				else if (rect.top == "50%") {
-					classes += ' rotate-counter-90';
+					classes += ' rotate-clock-90';
 				}
 
 				return classes;
