1bb18c3091174ab18633bbe74a94d9d07df62437
methodswithclass <chris@methodswithclass.com>
2015-07-03 23:36:49 -0400

    print children

--- features/nuplae/option.js
+++ features/nuplae/option.js
@@ -7,9 +7,15 @@ nuplaeModule.directive("option", ['nuplaeService', 'navigation', function (nuSer
 			info:'='
 		},
 		template:"<div class='relative width90 height-150 margin-v-50 center border pointer {{info.menu}}'>" +
-					"<div class='absolute center font-50 text-center white' id={{'title' + info.name}}>{{info.name}}</div>" + 
+					"<div class='absolute center font-50 text-center white'>{{info.name}}</div>" + 
 				 "</div>",
 		link:function ($scope, element, attr) {
+
+			var children = element[0].children;
+
+			for (i in children) {
+				console.log($(children[i]).attr('class'));
+			}
 
 			var info = $scope.info;
 
