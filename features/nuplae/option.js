ff352ed044043f89cb1dcb2303ff0397002df1c8
methodswithclass <chris@methodswithclass.com>
2015-07-03 22:17:17 -0400

    wide button

--- features/nuplae/option.js
+++ features/nuplae/option.js
@@ -6,7 +6,7 @@ nuplaeModule.directive("option", ['navigation', function (nav) {
 		scope:{
 			info:'='
 		},
-		template:"<div class='relative width80 height-200 margin-v-50 center border pointer {{info.menu}}' on-tap='tapped(info)'>" +
+		template:"<div class='relative width90 height-200 margin-v-50 center border pointer {{info.menu}}' on-tap='tapped(info)'>" +
 					"<div class='absolute center font-40 text-center white'>{{info.name}}</div>" + 
 				 "</div>",
 		link:function ($scope, element, attr) {
