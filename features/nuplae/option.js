117acc3e810df646f49cf22043933f2ceccb9c2b
methodswithclass <chris@methodswithclass.com>
2015-07-03 23:41:32 -0400

    option inner id

--- features/nuplae/option.js
+++ features/nuplae/option.js
@@ -2,19 +2,14 @@ nuplaeModule.directive("option", ['nuplaeService', 'navigation', function (nuSer
 
 	return {
 		restrict:'E',
+		replace:true,
 		scope:{
 			info:'='
 		},
 		template:"<div class='relative width90 height-150 margin-v-50 center border pointer {{info.menu}}'>" +
-					"<div class='absolute center font-50 text-center white'>{{info.name}}</div>" + 
+					"<div ng-attr-id='{{'title' + info.name}}' class='absolute center font-50 text-center white'>{{info.name}}</div>" + 
 				 "</div>",
 		link:function ($scope, element, attr) {
-
-			var children = element[0].children;
-
-			for (i in children) {
-				console.log($(children[i]).attr('class'));
-			}
 
 			var info = $scope.info;
 
