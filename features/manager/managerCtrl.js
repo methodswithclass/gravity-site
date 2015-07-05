06d8eeec96962eef31cf888cb0dcf6d02273c918
methodswithclass <chris@methodswithclass.com>
2015-07-05 19:16:44 -0400

    setup log in controller

--- features/manager/managerCtrl.js
+++ features/manager/managerCtrl.js
@@ -1,11 +1,17 @@
 managerModule.controller("managerCtrl", ['$document', 'validate.wrapper', 'states', 'events', 'con', function ($document, checkDevice, states, events, con) {
 
+	console.log("open manager controller");
+
 	events.on("console", function () {
+
+		console.log("console event dispatch");
 
 		return con.isRegistered();
 	});
 
 	angular.element($document).ready(function () {
+
+		console.log("document ready");
 
 		con.register($("#consoleContainer"));
 	
