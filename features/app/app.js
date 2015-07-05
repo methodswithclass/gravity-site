8c7d27e2071ff59436a0e6939e3585d60df89a16
methodswithclass <chris@methodswithclass.com>
2015-07-05 01:04:07 -0400

    checking variable

--- features/app/app.js
+++ features/app/app.js
@@ -1,5 +1,9 @@
 var app = angular.module('nuplae', ['sharedModule', 'nuplaeModule', 'consoleModule', 'accelModule', 'ngRoute', 'ui.router']);
 
+var checking = "/checking";
+var invalid = "/invalid";
+var valid = "/valid";
+
 app.run(function ($location, routeService, stateManager) {
 
     routeService.setup();
