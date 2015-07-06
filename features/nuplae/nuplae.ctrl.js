b8dd9061bb7235c9d4b0c71cc41fd5605f6446ee
methodswithclass <chris@methodswithclass.com>
2015-07-06 03:10:43 -0400

    event go to home

--- features/nuplae/nuplae.ctrl.js
+++ features/nuplae/nuplae.ctrl.js
@@ -25,6 +25,22 @@ nuplaeModule.controller('nuplaeCtrl', ['$document', 'params', 'validate.wrapper'
 	
 	});
 
+	events.on("loaded", function () {
+
+		console.log("loaded event dispatch");
+
+		var result = nuplaeService.parseInput(0);
+
+		result.then(
+		function (output) {
+			console.log("go to home");
+			states.gotoPage(output.index);
+		},
+		function (message) {
+			console.log(message);
+		});
+	})
+
 	states.define();
 
 	var result = checkDevice.run();
@@ -41,15 +57,7 @@ nuplaeModule.controller('nuplaeCtrl', ['$document', 'params', 'validate.wrapper'
 		$location.path(path);
 	});
 
-	var result = nuplaeService.parseInput(0);
-
-	result.then(
-	function (output) {
-		states.gotoPage(output.index);
-	},
-	function (message) {
-		console.log(message);
-	});
+	
 
 	
 
---
+++ features/nuplae/repeat.directive.js
@@ -0,0 +1,10 @@
+nuplaeModule.directive("repeat", ['events', function (events) {
+
+	return function (scope, element, attr) {
+		
+		if(scope.$last) {
+
+			events.dispatch("loaded");
+		}
+	}
+})
