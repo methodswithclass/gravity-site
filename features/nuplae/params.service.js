8e03a26b89a406537bedc88247b1e7a94c2e6631
methodswithclass <chris@methodswithclass.com>
2015-07-07 18:58:11 -0400

    back padding

--- features/nuplae/params.service.js
+++ features/nuplae/params.service.js
@@ -25,7 +25,8 @@ nuplaeModule.factory("params", function (global) {
 			menu:"pink-back",
 			button:"transparent",
 			backButton:{
-				loc:{'top':"10px", 'right':"10px", 'padding':"10px 0 10px 10px"},
+				loc:{top:"10px", right:"10px", padding:"10px 0 10px 10px"},
+				padding:'padding-right',
 				icon:"right"
 			},
 			rect:{
@@ -63,7 +64,7 @@ nuplaeModule.factory("params", function (global) {
 			menu:"green2-back",
 			button:"transparent",
 			backButton:{
-				loc:{'top':"10px", 'left':"10px", 'padding':"10px 10px 10px 0"},
+				loc:{top:"10px", left:"10px", padding:"10px 10px 10px 0"},
 				icon:"left"
 			},
 			rect:{
@@ -101,7 +102,7 @@ nuplaeModule.factory("params", function (global) {
 			menu:"brown-back",
 			button:"transparent",
 			backButton:{
-				loc:{'top':"10px", 'left':"10px", 'padding':"10px 10px 10px 0"},
+				loc:{top:"10px", left:"10px", padding:"10px 10px 10px 0"},
 				icon:"left"
 			},
 			rect:{
@@ -139,7 +140,7 @@ nuplaeModule.factory("params", function (global) {
 			menu:"blue2-back",
 			button:"transparent",
 			backButton:{
-				loc:{'top':"10px", 'right':"10px", 'padding':"0 10px 10px 10px"},
+				loc:{top:"10px", right:"10px", padding:"0 10px 10px 10px"},
 				icon:"up"
 			},
 			rect:{
