67f03eea50ccf207a263cee232e20251250cb6c3
methodswithclass <chris@methodswithclass.com>
2015-07-06 22:49:00 -0400

    remove nav from states

--- features/nuplae/states.js
+++ features/nuplae/states.js
@@ -37,7 +37,7 @@ nuplaeModule.factory("states", ['$state', '$rootScope', function ($state, $rootS
 	   	if (toState.name.split(".")[0] == "Page") {
 
 	   		console.log("go to current index: " + self.currentIndex);      
-	    	/nav.open(self.currentIndex, 500);
+	    	//nav.open(self.currentIndex, 500);
 		}
 	});
 
