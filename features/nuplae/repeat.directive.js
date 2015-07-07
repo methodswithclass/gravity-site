f0ad82b951d7a94f12b0d431794f03550af92f6d
methodswithclass <chris@methodswithclass.com>
2015-07-07 02:17:23 -0400

    log id

--- features/nuplae/repeat.directive.js
+++ features/nuplae/repeat.directive.js
@@ -2,6 +2,8 @@ nuplaeModule.directive("repeat", ['events', 'send', function (events, send) {
 
 	return function (scope, element, attr) {
 
+		console.log(attr.id);
+
 		send.accum({name:"pages", id:attr.id, data:element[0]})
 		
 		if(scope.$last) {
