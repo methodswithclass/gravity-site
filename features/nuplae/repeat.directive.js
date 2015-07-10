e0b8a47d96d651714fd9a101456c25f3e05229a3
methodswithclass <chris@methodswithclass.com>
2015-07-09 21:40:30 -0400

    move send into pages check

--- features/nuplae/repeat.directive.js
+++ features/nuplae/repeat.directive.js
@@ -3,10 +3,10 @@ nuplaeModule.directive("repeat", ['events', 'send', function (events, send) {
 	return function (scope, element, attr) {
 
 		//console.log(attr.id);
-
-		send.accum({name:attr.dir, id:attr.id, data:element[0]})
 		
 		if(scope.$last && attr.dir == "pages") {
+
+			send.accum({name:attr.dir, id:attr.id, data:element[0]})
 
 			var wait = setTimeout(function () {
 
