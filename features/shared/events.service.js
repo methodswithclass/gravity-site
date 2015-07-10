ac1d31ae68a99b32fe2d04fd3b0dc12d8659751e
methodswithclass <chris@methodswithclass.com>
2015-07-10 15:58:42 -0400

    revert event

--- features/shared/events.service.js
+++ features/shared/events.service.js
@@ -3,47 +3,15 @@ sharedModule.factory("events", function () {
 	var self = this;
 
 	this.events = {};
-	this.names = {};
-
-	var checkName = function (_name) {
-
-		for (i in names) {
-
-			if (_name == names[i]) {
-
-				return true;
-			}
-		}
-
-		return false;
-	}
 
 	var dispatch = function (name) {
 
-		var bin = self.events[name];
-
-		for (i in bin) {
-			return bin[i]();
-		}
+		return self.events[name]();
 	}
 
 	var on = function (name, _event) {
 
-		var bin;
-
-		if (!checkName(name)) {
-
-			bin = []; //create new receiver array for this name
-		}
-		else {
-			bin = self.events[name]; // retrieve existing receiver array for this name
-		}
-
-		bin[bin.length] = _event;
-
-		self.events[name] = bin;
-
-		self.names[self.names.length] = name;
+		self.events[name] = _event;
 	}
 
 	return {
