8e8a3efa8f6ff6dc484d6f03548e91eb0372f6fe
methodswithclass <chris@methodswithclass.com>
2015-07-09 22:24:23 -0400

    on press options

--- features/shared/on-press.directive.js
+++ features/shared/on-press.directive.js
@@ -2,10 +2,8 @@ sharedModule.directive('onPress', function () {
 	return function (scope, element, attrs) {
 		return $(element).hammer({
 			 	prevent_default: false,
-			 	drag_vertical: false,
-			 	time:1,
-			 	threshold:10
-			})
+			 	drag_vertical: false
+			}).get("press").set({time:1, threshold:10})
 			 .bind("press", function (ev) {
 			   return scope.$apply(attrs['onPress']);
 			 });
