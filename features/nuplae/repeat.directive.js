0e59c94b77e85a232f5674e82fad725a575fb31e
methodswithclass <chris@methodswithclass.com>
2015-07-09 21:56:06 -0400

    update repeat directive

--- features/nuplae/repeat.directive.js
+++ features/nuplae/repeat.directive.js
@@ -4,10 +4,10 @@ nuplaeModule.directive("repeat", ['events', 'send', function (events, send) {
 
 		//console.log(attr.id);
 
+		send.accum({name:attr.dir, id:attr.id, data:element[0]});
+
 		if(scope.$last && attr.dir == "pages") {
-
-			send.accum({name:attr.dir, id:attr.id, data:element[0]});
-
+			
 			var wait = setTimeout(function () {
 
 				events.dispatch("loaded");
