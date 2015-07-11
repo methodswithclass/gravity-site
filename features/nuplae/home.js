321d2b8610e73de1121e8162519bdbaea150bee1
methodswithclass <chris@methodswithclass.com>
2015-07-11 01:34:28 -0400

    update home

--- features/nuplae/home.js
+++ features/nuplae/home.js
@@ -7,7 +7,7 @@ nuplaeModule.directive("home", ['nuplaeService', 'send', function (nuServ, send)
 
 		$scope.scrollTop = function () {
 
-			return $(element).offset().top;
+			return element.offset().top;
 		}
 
 		$scope.$watch($scope.scrollTop,
