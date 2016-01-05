96ef40d5ab4a5acabc6492e166760483dbad598b
methodswithclass <chris@methodswithclass.com>
2016-01-05 04:00:36 -0500

    update

--- features/utility/utility.module.js
+++ features/utility/utility.module.js
@@ -30,7 +30,7 @@ angular.module("utility.module", ['sharedModule', 'accelModule'])
 		body:"body",
 		option:"option",
 		home:"home",
-		dist:20
+		dist:10
 	}
 
 	var setFactor = function (_factor) {
