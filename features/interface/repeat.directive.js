58ed8c803d7a998c6a9cc7c1627c0a524f23f324
methodswithclass <chris@methodswithclass.com>
2016-01-05 03:08:56 -0500

    update

--- features/interface/repeat.directive.js
+++ features/interface/repeat.directive.js
@@ -5,14 +5,6 @@ uiModule.directive("repeat", ['events', 'send', function (events, send) {
 		//console.log(attr.id);
 
 		send.retrieve.accum({name:attr.dir, id:attr.id, data:element[0]});
-
-		if(scope.$last && attr.dir == "pages") {
-			
-			var wait = setTimeout(function () {
-
-				events.dispatch("loaded");
-			}, 300);
-			
-		}
+		
 	}
 }]);
