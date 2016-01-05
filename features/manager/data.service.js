2008180716ba5730d70d1f38899336988628a36d
methodswithclass <chris@methodswithclass.com>
2016-01-05 03:40:56 -0500

    update

--- features/manager/data.service.js
+++ features/manager/data.service.js
@@ -114,7 +114,7 @@ managerModule.factory("data.service", ['utility', function (g) {
 		params:{
 			interval:1/600,
 			filterSize:3,
-			factor:1,
+			factor:0.8,
 			mu:0.1,
 			damp:0.4,
 			gravity:true,
@@ -156,7 +156,7 @@ managerModule.factory("data.service", ['utility', function (g) {
 		params:{
 			interval:1/300,
 			filterSize:3,
-			factor:0.8,
+			factor:1,
 			mu:0.06,
 			damp:0.4,
 			gravity:false,
@@ -240,7 +240,7 @@ managerModule.factory("data.service", ['utility', function (g) {
 		params:{
 			interval:1/300,
 			filterSize:2,
-			factor:0.6,
+			factor:0.8,
 			mu:0.05,
 			damp:0.4,
 			gravity:true,
@@ -283,7 +283,7 @@ managerModule.factory("data.service", ['utility', function (g) {
 		params:{
 			interval:1/300,
 			filterSize:3,
-			factor:0.2,
+			factor:0.4,
 			mu:0.8,
 			damp:0.4,
 			gravity:true,
