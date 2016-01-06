f427617c387fbb2a672a93723dbab3e7180bb26f
methodswithclass <chris@methodswithclass.com>
2016-01-05 19:18:13 -0500

    update

--- features/data/data.service.js
+++ features/data/data.service.js
@@ -154,9 +154,9 @@ dataModule.factory("data.service", ['utility', function (g) {
 			color:"black"
 		},
 		params:{
-			interval:2,
+			interval:3,
 			filterSize:5,
-			factor:2,
+			factor:5,
 			mu:0.1,
 			damp:0.4,
 			gravity:false,
