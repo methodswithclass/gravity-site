eb5b16ee8f67d2e6344432259d5ff26462bf40d2
methodswithclass <chris@methodswithclass.com>
2016-01-06 12:50:43 -0500

    update

--- features/enemy/preview.js
+++ features/enemy/preview.js
@@ -10,14 +10,14 @@ enemyModule.directive("preview", ['data.service', 'events', function (data, even
 			$scope.types = data.enemydata;
 
 			var width = $(window).width()*0.9;
-			var height = $(window).height()*0.7;
+			var height = $(window).height()*0.9;
 
 			if (width > height) width = height;
 			else if (height > width) height = width;
 
 			setTimeout(function () {
 
-				$(element[0].querySelector("#previewenemies")).css({width:width*1.2, height:height});
+				$(element[0].querySelector("#previewenemies")).css({width:width, height:height*0.6});
 			}, 1000);
 			
 			$scope.hideinfo = $scope.info.id != "enemies";
