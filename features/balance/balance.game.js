fe7d6f209683f378a3ca9c630ee15b759e3a2f16
methodswithclass <chris@methodswithclass.com>
2016-01-08 03:20:31 -0500

    update

--- features/balance/balance.game.js
+++ features/balance/balance.game.js
@@ -60,7 +60,7 @@ balanceModule.factory("balance.game", ['target.service', 'keeper', 'global', 've
 
 		//console.log(keeper);
 
-		keeper.tick(interval);
+		keeper.tick(interval*5);
 
 		if (target.active) {
 
