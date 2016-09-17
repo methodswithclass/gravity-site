10da923556c1e8bb38bf1292b7020342d124c8df
Christopher Polito <chris@methodswithclass.com>
2016-09-16 20:37:27 -0400

    update

--- features/enemy/enemy.game.js
+++ features/enemy/enemy.game.js
@@ -1,6 +1,6 @@
 enemyModule.factory("enemy.game", ['enemy.service', 'keeper', 'global', function(Enemy, keeperFactory, g) {
 
-	var total = 5;
+	var total = 8;
 	var enemies = [];
 	var keeper;
 	var timer;
