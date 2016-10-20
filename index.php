53f99f4c74fb1debbc476e82c42a99c4a3ff24cd
Christopher Polito <chris@methodswithclass.com>
2016-10-20 09:39:38 -0400

    update

--- index.php
+++ index.php
@@ -32,6 +32,17 @@
 
     <base href="/">
 
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-85962895-3', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+
   </head>
   <body ng-app="gravity">
     <div class="absolute width height deselect" id="main">
