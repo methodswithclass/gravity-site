be4330f072e7a5e0904f8e69ca63864998f590de
Christopher Polito <chris@methodswithclass.com>
2016-10-23 12:33:28 -0400

    update

--- features/games/display.js
+++ features/games/display.js
@@ -18,7 +18,7 @@ gamesModule.directive("display", ['send', function (send) {
 
 				var display = {time:time, points:points, stats:stats};
 
-				console.log("get display", page.id, time[0], points[0], stats[0])
+				//console.log("get display", page.id, time[0], points[0], stats[0])
 
 				send.retrieve.accum({name:"display", id:page.id, data:display});
 
