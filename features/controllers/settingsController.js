e64c787c7ebf9e45738dbfc8fcf0c67769598a03
Christopher Polito <chris@methodswithclass.com>
2016-12-08 11:33:03 -0500

    update

--- features/controllers/settingsController.js
+++ features/controllers/settingsController.js
@@ -6,7 +6,54 @@ controllerModule.controller("SettingsController", ['$scope', 'global', 'states',
 
 	//console.log(" ");
 	console.log("settings controller");
-	
+
+
+    var panels = [
+        "setting-sessionfactor",
+        "setting-axes",
+        "setting-3",
+        "setting-4",
+        "setting-5",
+        "setting-6",
+        "setting-7",
+        "setting-8",
+        "setting-9"
+    ]
+
+    var makeSizes = function () {
+
+        var win = {
+            width:$(window).width(),
+            height:$(window).height()
+        }
+
+        var panel = {
+            width:win.width,
+            height:win.height
+        }
+
+        var pn = panels.length/3;
+        var ifac = 1.1;
+        var bf = 1.2;
+        var cf = 1.4;
+        var pf = 1;
+
+        for (i in panels) {
+
+            $("#" + panels[i]).css({width:panel.width*pf, height:panel.height*pf});
+        }
+
+        $("#settings-inner").css({width:win.width*pn*ifac, height:win.height*pn*ifac});
+        $("#settings-body").css({width:win.width*pn*bf, height:win.height*pn*bf});
+        $("#settings-container").css({width:win.width*pn*cf, height:win.height*pn*cf});
+
+    }   
+
+
+    setTimeout(function () {
+        makeSizes();
+    }, 500)
+
 
 
     $scope.xswitched = utility.deviceStandard.standard;
