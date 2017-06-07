810e1fa69e52bba75567fd3b8c8f4a1620793366
Christopher Polito <chris@polito.io>
2017-06-06 20:33:34 -0400

    update

--- src/components/interface/display.directive.js
+++ src/components/interface/display.directive.js
@@ -10,19 +10,19 @@ interfaceModule.directive("display", ['send.service', function (send) {
 
 			var page = $scope.page;
 
-			// setTimeout(function() {
+			setTimeout(function() {
 
-			// 	var time = $("#time" + page.id);
-			// 	var points = $("#points" + page.id);
-			// 	var stats = $("#stats" + page.id);
+				var time = $("#time" + page.id);
+				var points = $("#points" + page.id);
+				var stats = $("#stats" + page.id);
 
-			// 	var display = {time:time, points:points, stats:stats};
+				var display = {time:time, points:points, stats:stats};
 
-			// 	//console.log("get display", page.id, time[0], points[0], stats[0])
+				//console.log("get display", page.id, time[0], points[0], stats[0])
 
-			// 	send.retrieve.accum({name:"display", id:page.id, data:display});
+				send.retrieve.accum({name:"display", id:page.id, data:display});
 
-			// }, 2000);
+			}, 2000);
 
 		}
 	}
