51b3b732241d94c38c4161484118180793e84e00
Christopher Polito <chris@polito.io>
2017-08-11 16:50:26 -0400

    update

--- src/components/controllers/settings.axes.controller.js
+++ src/components/controllers/settings.axes.controller.js
@@ -5,20 +5,7 @@ controllerModule.controller("settings.axes.controller", ['$scope', 'global.servi
 
     var util = mcaccel.utility;
 
-    $scope.settings = data.getPageById("settings").settings
-    // $scope.xswitched = settings.getDirState(util.getAxis(util.const.x)) ? utility.deviceStandard.switched : utility.deviceStandard.standard;
-    // $scope.yswitched = settings.getDirState(util.getAxis(util.const.y)) ? utility.deviceStandard.switched : utility.deviceStandard.standard;
-
-    // var calibration = {};
-    // calibration.x;
-    // calibration.y;
-
-
-    // var getCalibration = function () {
-    //
-    //     calibration.x = util.getAxis(util.const.x);
-    //     calibration.y = util.getAxis(util.const.y);
-    // }
+    $scope.settings = data.getPageById("settings").settings;
 
     $scope.axesDir = {};
     $scope.axesDir.x = settings.getDirState(util.getAxis(util.const.x));
@@ -26,7 +13,6 @@ controllerModule.controller("settings.axes.controller", ['$scope', 'global.servi
 
     if (!settings.settings.direction.axesSet()) {
         settings.settings.direction.axesSet(true);
-        // getCalibration();
         $scope.axesDir.x = false;
         $scope.axesDir.y = false;
     }
@@ -37,12 +23,8 @@ controllerModule.controller("settings.axes.controller", ['$scope', 'global.servi
         $scope.switched.x = $scope.axesDir.x ? "switched" : "standard";
         $scope.switched.y = $scope.axesDir.y ? "switched" : "standard";
 
-        // calibration.x = settings.getStateDir($scope.axesDir.x);
-        // calibration.y = settings.getStateDir($scope.axesDir.y);
-
     }
-
-
+    
     setSwitched();
 
     $scope.setDirection = function (axis) {
@@ -51,13 +33,8 @@ controllerModule.controller("settings.axes.controller", ['$scope', 'global.servi
 
         setSwitched();
 
-        settings.settings.direction.setOverride(axis, settings.getStateDir(axis === "i" ? $scope.axesDir.x : $scope.axesDir.y));
-
-        // var native = settings.getStateDir(axis === "i" ? calibration.x : calibration.y);
-        // var override =
-        // var direction = native*override;
-
-        // settings.settings.direction.changeDirection(axis, settings.getStateDir(axis === "i" ? $scope.axesDir.x : $scope.axesDir.y));
+        // settings.settings.direction.setOverride(axis, settings.getStateDir(axis === "i" ? $scope.axesDir.x : $scope.axesDir.y));
+        settings.settings.direction.setOverride(axis, -1);
     }
 
 
