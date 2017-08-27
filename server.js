870446f18490e7b5ee6c09ed3edf6ba278277fd4
Christopher Polito <chris@polito.io>
2017-08-27 15:30:45 -0400

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 
-const middle = require("./server/middleware");
+const middle = require("./server/middleware.js");
 
 const app = express();
 
