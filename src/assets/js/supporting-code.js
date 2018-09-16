5c8fd52e471701efc97dbcc8d6ec9adcc7d0a2b7
Christopher Polito <cpolito343@gmail.com>
2018-09-16 08:37:23 -0400

    update

---
+++ src/assets/js/supporting-code.js
@@ -0,0 +1,60 @@
+
+
+
+
+var getAngularModules = function (application) {
+
+	// console.log("application is", application);
+
+	application.directive('onTap', function () {
+		return function (scope, element, attrs) {
+			return $(element).hammer({
+				    prevent_default: true,
+				    time:1
+				})
+				.bind("tap", function (ev) {
+				    return scope.$apply(attrs['onTap']);
+				});
+		};
+	});
+
+
+
+	application.factory("general", ["$sce", function ($sce) {
+
+
+		return {
+			renderHtml:function (htmlCode) {
+	        	return $sce.trustAsHtml(htmlCode);
+	    	}
+		}
+
+	}])
+
+
+
+	application.factory('exception', [function () {
+	    
+
+
+	    var catcher = function (message) {
+	        return function(reason) {
+	            console.log(message, reason);
+	        };
+	    }
+
+
+	    return {
+	        catcher: catcher
+	    };
+	}]);
+
+
+	window.onerror = function (msg, url, lineNo, columnNo, error) {
+	  // var error = e.error;
+	  captureError(error);
+
+	  return true;
+	}
+
+}
