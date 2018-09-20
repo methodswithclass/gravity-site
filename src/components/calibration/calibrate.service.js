b18ace87a1108839daa576d361ae916a90c141f3
Christopher Polito <cpolito343@gmail.com>
2018-09-20 06:49:37 -0400

    update

--- src/components/calibration/calibrate.service.js
+++ src/components/calibration/calibrate.service.js
@@ -2,7 +2,7 @@ calibrateModule.factory("calibrate.service", ['progress.service', 'events', 'toa
 
     var g = mcaccel.utility;
 
-    g.setDist(20);
+    g.setDist(25);
 
     var accel;
     var obj;
