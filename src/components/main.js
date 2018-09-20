d91d4a3b14f9ea1c26fedc3dd24ff0eb27cdd1c6
Christopher Polito <cpolito343@gmail.com>
2018-09-19 22:09:30 -0400

    update

--- src/components/main.js
+++ src/components/main.js
@@ -15,7 +15,7 @@ app.config(['state.providerProvider', '$locationProvider', function (stateProvid
 }]).run(["state.service", "utility.service", function (states, utility) {
 
 	//for desktop debugging when no accelerometer is present, comment out this line for production
-    // utility.forceValidity(true);
+    utility.forceValidity(true);
     
 
     //1. check existence of device accelerometer
